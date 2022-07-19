a2acb478fec276f8f1389f708c0be02f2c689090
shashank.maiya <shashank.maiya@servicenow.com>
2022-07-18 17:05:32 -0700

    comments

--- LeetCode_python/src/other_algorithms/rangeSum_SegmentTree_M.py
+++ LeetCode_python/src/other_algorithms/rangeSum_SegmentTree_M.py
@@ -1,4 +1,6 @@
 '''
+307. Range Sum Query - Mutable
+
 Question: Given an integer array nums, find the sum of the elements between indices i and j (i <= j), inclusive.
 The update(i, val) function modifies nums by updating the element at index i to val.
 Given nums = [1, 3, 5]
